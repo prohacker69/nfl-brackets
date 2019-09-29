f6c2c5b188b8f46e370427c8ae38d34f7aaf5f38
Glitch (hello-webpage) <none>
2019-09-29 12:23:58 +0000

    ✌️🚂 Checkpoint ./script.js:3032978/218

--- script.js
+++ script.js
@@ -1,20 +1,18 @@
 let button = document.querySelector("#submit")
 button.addEventListener("click",()=>{
-  console.log("You clicked me!")
   let team = document.querySelector("#input").value
   firebase.database().ref("/").push(team)
 loadTeams()
 })
 
 function loadTeams(){
-  console.log('hi')
+  document.querySelector(''.list").innerHTML= ''
   firebase.database().ref("/").on("value",(snapshot)=>{
- console.log(snapshot)
   let data = snapshot.val()
-console.log(data)
     for(let keys in data) {
       document.querySelector(".list").innerHTML += `<div>${data[keys]}</div>`
     }
 })
 }
 
+window.onload = loadTeams 
