1533be5acd3a66edab0182c06dd6c8d67d263bc9
Glitch (hello-webpage) <none>
2019-10-13 20:42:19 +0000

    🌻🏭 Checkpoint ./index.html:3032978/334 ./script.js:3032978/3476

--- script.js
+++ script.js
@@ -1,33 +1,35 @@
 let button = document.querySelector("#submit")
-
+let db = firebase.firestore()
 button.addEventListener("click",()=>{
   let team = document.querySelector("#input").value
-  firebase.database().ref("/").push(te9am)
+  db.collection("teams").add({team})
 loadTeams()
   document.querySelector("#input").value = ""
 })
 
 function loadTeams(){
   document.querySelector(".list").innerHTML= ''
-  firebase.database().ref("/").on("value",(snapshot)=>{
-  let data = snapshot.val()
-    for(let keys in data) {
-      document.querySelector(".list").innerHTML += `
-    <div class="col s12 m6">
-      <div class="card blue-grey darken-1">
-        <div class="card-content white-text">
-          <span class="card-title">${data[keys]}</span>
-        </div>
-        <div class="card-action">
-          <a href="#" class="upvote">upvote</a>
+  let allTeams = db.collection("teams").get()
+  console.log(allTeams)
+//   firebase.database().ref("/").on("value",(snapshot)=>{
+//   let data = snapshot.val()
+//     for(let keys in data) {
+//       document.querySelector(".list").innerHTML += `
+//     <div class="col s12 m6">
+//       <div class="card blue-grey darken-1">
+//         <div class="card-content white-text">
+//           <span class="card-title">${data[keys]}</span>
+//         </div>
+//         <div class="card-action">
+//           <a href="#" class="upvote">upvote</a>
         
-        </div>
-      </div>
-    </div>`
-    }
-    loadUpvoteButtons()
+//         </div>
+//       </div>
+//     </div>`
+//     }
+//     loadUpvoteButtons()
     
-})
+// })
 }
 function loadUpvoteButtons(){
 let upvoteButtons = document.querySelectorAll(".upvote")
